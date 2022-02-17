1c698847660e3ba53791b5ed17534352c524c6cc
khoirul anwar <khoirula964@gmail.com>
2022-02-17 21:54:12 +0700

    hilangkan field yang tidak perlu

--- backendapp/account/admin.py
+++ backendapp/account/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'email', 'fullname', 'phone', 'photo', 
+                'role', 'password'
+            )
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser'
+            )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields':(
+                'email', 'fullname', 'phone', 'photo', 
+                'role', 'password1', 'password2',
+            )
+        }),
+    )
+    list_display = ['created_at', 'role', 'email', 'fullname']
+    search_fields = ['role', 'email', 'fullname']
+    ordering = ['-created_at']
+
+admin.site.site_header = "STEM MANAGEMENT ADMIN"
+admin.site.register(CustomUser, UserAdmin)
--- backendapp/account/models.py
+++ backendapp/account/models.py
@@ -35,38 +35,20 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     email = models.EmailField(max_length=255, unique=True)
-    name = models.CharField(max_length=255, default='')
-    about = models.TextField(blank=True, null=True)
-    location = models.CharField(max_length=255, blank=True, null=True)
+    fullname = models.CharField(max_length=255, default='')
     phone = models.CharField(max_length=20, default='')
     photo = models.ImageField(upload_to='user/photo/', blank=True, null=True)
-    LISTGENDER = (
-        ('Male', 'Male'),
-        ('Female', 'Female'),
-    )
-    gender = models.CharField(max_length=100, choices=LISTGENDER, null=True)
-    fb_url = models.CharField(max_length=255, blank=True, null=True)
-    tw_url = models.CharField(max_length=255, blank=True, null=True)
-    insta_url = models.CharField(max_length=255, blank=True, null=True)
-    in_url = models.CharField(max_length=255, blank=True, null=True)
-    as_teacher_apply = models.BooleanField(default=False)
-    as_teacher_confirmed = models.BooleanField(default=False)
-    as_teacher_confirmed_at = models.DateField(blank=True, null=True)
-    as_teacher_req_desc = models.TextField(blank=True, null=True)
     ROLES = (
+        ('Superadmin', 'Superadmin'),
         ('Admin', 'Admin'),
         ('Student', 'Student'),
-        ('Teacher', 'Teacher'),  # instructor
     )
-    role = models.CharField(max_length=100, choices=ROLES, default='Patient')
-    dob = models.DateField(blank=True, null=True)
-    nip = models.CharField(max_length=100, blank=True, null=True)
-    is_verified = models.BooleanField(default=False)
+    role = models.CharField(max_length=100, choices=ROLES, default='Student')
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'phone', 'gender', 'role', 'is_active', 'is_staff', 'nip']
+    REQUIRED_FIELDS = ['name', 'phone', 'role', 'is_active', 'is_staff', 'nip']
 
     objects = CustomUserManager()
 
