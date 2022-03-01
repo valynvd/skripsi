f7275bf1d970fb699d88eb96c70d8e50638ed986
Software Engineering Prasmul <softwareengineeringprasmul@Softwares-MacBook-Pro.local>
2022-03-01 13:16:47 +0700

    change null header token

--- frontend/src/utils/base/axios.js
+++ frontend/src/utils/base/axios.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { getToken } from '../helpers';
 
-export const defaultParams = () => ({
-  headers: { Authorization: `Token ${getToken()}` },
-});
+export const defaultParams = () => {
+  const token = getToken();
+  if (token) {
+    return ({
+      headers: { Authorization: `Token ${getToken()}` },
+    });
+  }
+  return ({});
+};
 
 export default axios;
