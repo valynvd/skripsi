108053cef3d2bcfbf5120e5d919ccaa58b67bcc8
unknown <vlukmansyah20900@gmail.com>
2023-03-15 13:50:16 +0700

    update logo and change name

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,18 +1,20 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import NavigationLink from './NavigationLink';
 import { BiHome } from 'react-icons/bi';
 import { AiOutlineMail, AiOutlineFileText } from 'react-icons/ai';
-import { MdOutlineAssignment } from 'react-icons/md';
+import { MdOutlineAssignment, MdKeyboardArrowLeft } from 'react-icons/md';
 
 const Navbar = () => {
   return (
     <div className="w-[20rem] h-full flex flex-col">
-      <div className="h-[5.5rem] bg-primary-400 flex items-center justify-center">
+      <div className="h-[5.5rem] py-4 bg-primary-400 flex items-center justify-center relative">
         <img
-          className="w-[72%]"
+          className="w-[72%] h-full object-contain"
           src={require('../../assets/logo/prasmul-logo-white.png')}
           alt="prasmul"
         />
+        <div className="absolute w-[2.5rem] rounded-full h-[2.5rem] bg-gray-200 border-2 border-white right-0 translate-x-[50%]"></div>
       </div>
       <nav className="flex-1 bg-secondary-400">
         <div className="pt-6">
