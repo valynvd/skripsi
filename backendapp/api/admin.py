8b9aea4ade88a38800e2674ed233cafee063da96
unknown <vlukmansyah20900@gmail.com>
2023-06-07 22:03:57 +0700

    update

--- backendapp/api/admin.py
+++ backendapp/api/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 from . import models
 
+class MataKuliahAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+  pass
+
 # Register your models here.
 admin.site.register(models.Kurikulum)
-admin.site.register(models.MataKuliah)
+admin.site.register(models.MataKuliah, MataKuliahAdmin)
 admin.site.register(models.Dosen)
 admin.site.register(models.ProgramStudi)
 admin.site.register(models.PenugasanPengajaran)
