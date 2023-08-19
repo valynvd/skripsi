807170b942a15f02b0be3fbcb20f89a5095a951f
unknown <vlukmansyah20900@gmail.com>
2023-08-20 01:43:37 +0700

    update

--- backendapp/akreditasi/models.py
+++ backendapp/akreditasi/models.py
@@ -22,6 +22,7 @@ class PoinPenilaian(models.Model):
         blank=True,
         null=True
 	)
+    year = models.IntegerField(blank=True, null=True)
     type = models.CharField(max_length=10, blank=True, null=True)
     order_number = models.IntegerField(blank=True, null=True)
     item_number = models.CharField(max_length=20, blank=True, null=True)
@@ -32,11 +33,19 @@ class PoinPenilaian(models.Model):
     description_grade_2 = models.TextField(blank=True, null=True)
     description_grade_3 = models.TextField(blank=True, null=True)
     description_grade_4 = models.TextField(blank=True, null=True)
-    score = models.FloatField(default=0)
-    dokumenPendukung = models.ManyToManyField(SuratPenugasan, blank=True)
 
     def __str__(self) -> str:
         return '{}-{}'.format(self.element, self.kriteriaId.nama)
+
+class RiwayatPoinPenilaian(models.Model):
+    kriteriaId = models.ForeignKey(
+        PoinPenilaian,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+	)
+    score = models.FloatField(default=0)
+    dokumenPendukungSuratPenugasan = models.ManyToManyField(SuratPenugasan, blank=True)
 
 class FileFolder(models.Model):
     matrix = models.ForeignKey(
