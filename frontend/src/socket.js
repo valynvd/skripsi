90f6cb7b5f9ba67bffcbbc7f09c3e2757b0fe481
Gaizka Valencia <31756055+gvlencia@users.noreply.github.com>
2023-08-21 09:51:16 +0700

    socket secure true

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import socketIO from 'socket.io-client';
 
 // const URL = socketIO.connect('http://localhost:4000');
-const URL = socketIO.connect('https://stembot.vercel.app');
+const URL = socketIO.connect('https://stembot.vercel.app', {secure: true});
 
 export const socket = URL;
