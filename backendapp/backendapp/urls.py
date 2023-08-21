d644fa5085060d93c925340e7b7dd8abc25a462a
unknown <vlukmansyah20900@gmail.com>
2023-08-21 12:18:32 +0700

    update

--- backendapp/backendapp/urls.py
+++ backendapp/backendapp/urls.py
@@ -22,13 +22,13 @@ admin.site.site_header = 'SIMANTAB Prodi Software Engineering'
 
 urlpatterns = [
     #  # API
-    path('api-stem/', include('api.urls')),
+    path('simantap/api-stem/', include('api.urls')),
 
     # auth djoser
     # auth token
-    path('auth-stem/', include('djoser.urls')),
-    path('auth-stem/', include('djoser.urls.authtoken')),
-    path('admin-stem/', admin.site.urls),
+    path('simantap/auth-stem/', include('djoser.urls')),
+    path('simantap/auth-stem/', include('djoser.urls.authtoken')),
+    path('simantap/admin-stem/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
