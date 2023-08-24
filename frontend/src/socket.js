93bdd8190bfe650216c580e30a6e59d0a3527d85
Gaizka Valencia <31756055+gvlencia@users.noreply.github.com>
2023-08-25 05:42:18 +0700

    socket

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import socketIO from 'socket.io-client';
 
-const URL = socketIO.connect('http://localhost:8080');
-// const URL = socketIO.connect('https://stembot.fly.dev');
+// const URL = socketIO.connect('http://localhost:4000');
+const URL = socketIO.connect('https://stembot.fly.dev');
 
 export const socket = URL;
